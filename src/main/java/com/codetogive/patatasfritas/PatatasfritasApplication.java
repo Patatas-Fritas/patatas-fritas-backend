3f60004e8a535bb6429065a1fb9409c209a5232a
Rita Csullag <csullagrita@gmail.com>
2021-06-02 19:47:34 +0200

    Initialize spring boot project

---
+++ src/main/java/com/codetogive/patatasfritas/PatatasfritasApplication.java
@@ -0,0 +1,13 @@
+package com.codetogive.patatasfritas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatatasfritasApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PatatasfritasApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/codetogive/patatasfritas/PatatasfritasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetogive.patatasfritas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatatasfritasApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
