64348738759881e93c13805b2c6c84d4659c343d
Rita Csullag <csullagrita@gmail.com>
2021-06-06 13:43:13 +0200

    Add crossOrigin annotation to controller

--- src/main/java/com/codetogive/patatasfritas/users/UserController.java
+++ src/main/java/com/codetogive/patatasfritas/users/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.codetogive.patatasfritas.users.dtos.UserRequestDTO;
 import com.codetogive.patatasfritas.users.exceptions.OccupiedUsernameException;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class UserController {
 
