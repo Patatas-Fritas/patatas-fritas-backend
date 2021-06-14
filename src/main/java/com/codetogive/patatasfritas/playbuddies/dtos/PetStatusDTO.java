0959afb396ce6582d3962651c6d36d884a558781
Pepo-Fabian Aniko <aniko.fabian15@gmail.com>
2021-06-14 20:49:42 +0200

    time handling added to PetStatusDTO

--- src/main/java/com/codetogive/patatasfritas/playbuddies/dtos/PetStatusDTO.java
+++ src/main/java/com/codetogive/patatasfritas/playbuddies/dtos/PetStatusDTO.java
@@ -1,9 +1,10 @@
 package com.codetogive.patatasfritas.playbuddies.dtos;
 
 import com.codetogive.patatasfritas.playbuddies.PlayBuddy;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
@@ -15,7 +16,14 @@ public class PetStatusDTO {
   private String petName;
   private Boolean isHungry;
 
-  public PetStatusDTO(PlayBuddy playBuddy){
-    //this.isHungry= ide kéne ez a 12 órás check
+  public PetStatusDTO(PlayBuddy playBuddy) {
+    this.petId = playBuddy.getId();
+    this.petName = playBuddy.getName();
+    this.isHungry = false;
+    int difference = 12 * 60 * 60 * 1000;
+    if (Timestamp.valueOf(LocalDateTime.now()).getTime() - playBuddy.getLastFeeding().getTime()
+        > difference) {
+      isHungry = true;
+    }
   }
 }
